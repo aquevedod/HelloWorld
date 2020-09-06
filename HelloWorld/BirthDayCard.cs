8ded610186665b1f0d2e81161f26a2fd69e53548
Arnold <aquevedod@outlook.com>
2020-09-05 19:44:27 -0500

    hola mundo

---
+++ HelloWorld/BirthDayCard.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    public class BirthDayCard
+    {
+        public int[] hackerCard(int[] collectionLeanne, int budgetMike)
+        {
+            int[] newCollection = null;
+            _ = new List<int>();
+            List<int> listLeanne = collectionLeanne.ToList();
+            List<int> superList = new List<int>();
+
+            double maxSizeCollection = Math.Pow(10, 9);
+
+            try
+            {
+                if (budgetMike <= 0 && budgetMike > maxSizeCollection)
+                    return newCollection;
+
+                if (collectionLeanne != null && collectionLeanne.Length > 0 && collectionLeanne.Length <= maxSizeCollection)
+                {
+                    Array.Sort(collectionLeanne);
+
+                    for (int i = 1; i <= budgetMike; i++)
+                    {
+                        superList.Add(i);
+                    }
+                    superList = RemoveValues(collectionLeanne, superList);
+                    List<int> newCollectionList = CalculateValues(superList, budgetMike);
+                    //listLeanne.AddRange(newCollectionList);
+                    //listLeanne.Sort();
+                    newCollection = newCollectionList.ToArray();
+                }
+                else
+                {
+                    newCollection = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return newCollection;
+        }
+
+        private List<int> RemoveValues(int[] list1, List<int> list2)
+        {
+            foreach (int item in list1)
+            {
+                list2.Remove(item);
+            }
+            return list2;
+        }
+
+        private List<int> CalculateValues(List<int> listCaculated, int budget)
+        {
+            int mount = 0;
+            List<int> finalList = new List<int>();
+            foreach (int item in listCaculated)
+            {
+                mount = mount + item;
+
+                if (mount <= budget)
+                {
+                    finalList.Add(item);
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return finalList;
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallhackerCard();
+        }
+
+        private static void CallhackerCard()
+        {
+            var obj = new BirthDayCard();
+            int[] collectionLeanne = new int[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+            int[] nuevoArray = obj.hackerCard(collectionLeanne, 20);
+
+            if (nuevoArray != null && nuevoArray.Length > 0)
+            {
+                foreach (int item in nuevoArray)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
